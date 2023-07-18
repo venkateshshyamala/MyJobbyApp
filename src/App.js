ee948c7abadb382d6ceed73fc2bf2892c1912816
venkateshshyamala <venkat.mai938@gmail.com>
2023-07-18 15:17:58 +0000

    .

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import {Switch, Route} from 'react-router-dom'
+
+import Login from './Components/Login'
+
+import Home from './Components/Home'
+
+import Jobs from './Components/Jobs'
+
+import JobItemDetails from './Components/JobItemDetails'
+
+import NotFound from './Components/NotFound'
+
+import ProtectedRoute from './Components/ProtectedRoute'
+
+import './App.css'
+
+// These are the lists used in the application. You can move them to any component needed.
+// eslint-disable-next-line no-unused-vars
+const employmentTypesList = [
+  {
+    label: 'Full Time',
+    employmentTypeId: 'FULLTIME',
+  },
+  {
+    label: 'Part Time',
+    employmentTypeId: 'PARTTIME',
+  },
+  {
+    label: 'Freelance',
+    employmentTypeId: 'FREELANCE',
+  },
+  {
+    label: 'Internship',
+    employmentTypeId: 'INTERNSHIP',
+  },
+]
+
+// eslint-disable-next-line no-unused-vars
+const salaryRangesList = [
+  {
+    salaryRangeId: '1000000',
+    label: '10 LPA and above',
+  },
+  {
+    salaryRangeId: '2000000',
+    label: '20 LPA and above',
+  },
+  {
+    salaryRangeId: '3000000',
+    label: '30 LPA and above',
+  },
+  {
+    salaryRangeId: '4000000',
+    label: '40 LPA and above',
+  },
+]
+
+// Replace your code here
+
+const App = () => (
+  <>
+    <Switch>
+      <Route exact path="/login" component={Login} />
+      <ProtectedRoute exact path="/" component={Home} />
+      <ProtectedRoute exact path="/jobs" component={Jobs} />
+      <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
+      <Route component={NotFound} />
+    </Switch>
+  </>
+)
+
+export default App
